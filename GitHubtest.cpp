4808918109aa6da5a05770106d774e4ac8619ace
takuma1001 <115197536+takuma1001@users.noreply.github.com>
2022-11-18 09:26:15 +0900

    リモートリポジトリでコンフリクト変更

--- GitHubtest.cpp
+++ GitHubtest.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git\n";
 
-    std::cout << "New branch changed\n";
+    std::cout << "GitHub Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
